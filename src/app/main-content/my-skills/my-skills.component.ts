7050761e30f10e8bc30e465979c573c4369f13ab
xCnKenan <cn.kenan@icloud.com>
2025-09-17 18:45:30 +0200

    first design of my skills component html

--- src/app/main-content/my-skills/my-skills.component.ts
+++ src/app/main-content/my-skills/my-skills.component.ts
@@ -1,12 +1,63 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './my-skills.component.html',
-  styleUrl: './my-skills.component.scss'
+  styleUrl: './my-skills.component.scss',
 })
 export class MySkillsComponent {
+  mySkills = [
+    {
+      img: 'html.png',
+      skill: 'HTML',
+    },
+    {
+      img: 'css.png',
+      skill: 'CSS',
+    },
+    {
+      img: 'javascript.png',
+      skill: 'JavaScript',
+    },
+    {
+      img: 'typescript.png',
+      skill: 'TypeScript',
+    },
+    {
+      img: 'angular.png',
+      skill: 'Angular',
+    },
+    {
+      img: 'firebase.png',
+      skill: 'Firebase',
+    },
+    {
+      img: 'git.png',
+      skill: 'Git',
+    },
+    {
+      img: 'rest-api.png',
+      skill: 'REST-API',
+    },
+    {
+      img: 'scrum.png',
+      skill: 'Scrum',
+    },
+    {
+      img: 'material-design.png',
+      skill: 'Material Design',
+    },
+    {
+      img: 'interest.svg',
+      skill: 'Continually learning',
+    },
+  ];
 
+  constructor() {
+    
+  }
 }
