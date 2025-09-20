416d52047742b890c88e044ed3788866d4c51e7d
xCnKenan <cn.kenan@icloud.com>
2025-09-20 16:00:03 +0200

    Kontaktformular: HTML der Form

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
+
+  constructor(){}
+
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+  }
+
+  onSubmit(){
+    console.log(this.contactData);
+    
+  }
 }
